a869f7ee8693d5f1050b2762e0eab6b04c5da401
Doum Kim <eha0129@naver.com>
2020-04-16 16:04:16 +0000

    Use for in loop

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-def plus(a,b):
-  if type(b) is int or type(b) is float:
-    return a+b
+# def plus(a,b):
+#   if type(b) is int or type(b) is float:
+#     return a+b
+#   else:
+#     return None
+
+# print(plus(12,10.4))
+
+# def age_check(age):
+#   print(f"you are {age}")
+#   if age < 18:
+#     print("You can't drink")
+#   elif age == 18:
+#     print("You are new to this!")
+#   elif age > 20 and age < 25:
+#     print("You are still kind of young")
+#   else:
+#     print("Enjoy your drink")
+
+# age_check(18)
+
+days = ('Mon','Tue','Wed','Thu','Fri')
+
+for day in days:
+  if day == "Wed":
+    break
   else:
-    return None
+    print(day)
 
-print(plus(12,10.4))
-
-def age_check(age):
-  print(f"you are {age}")
-  if age < 18:
-    print("You can't drink")
-  elif age == 18:
-    print("You are new to this!")
-  elif age > 20 and age < 25:
-    print("You are still kind of young")
-  else:
-    print("Enjoy your drink")
-
-age_check(23)
+for letter in "nicolas":
+  print(letter)
